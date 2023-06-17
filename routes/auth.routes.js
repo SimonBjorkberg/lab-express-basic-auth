674cc037774e6ecb2280b138239bf9e777253c92
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-06-17 12:04:11 +0200

    iteration 2 done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,69 @@
-const { Router } = require('express')
-const router = new Router
+const { Router } = require("express");
+const router = new Router();
 
-const User = require('../models/User.model')
+const User = require("../models/User.model");
 
-const bcrypt = require('bcryptjs')
-const salt = 10
+const bcrypt = require("bcryptjs");
+const salt = 10;
 
-router.get('/signup', (req, res, next) => {
-    res.render('./auth/sign-up')
-})
+router.get("/signup", (req, res, next) => {
+  res.render("./auth/sign-up");
+});
+
+router.post("/signup", (req, res, next) => {
+  let { username, password } = req.body;
+  User.findOne({ username }).then((existingUser) => {
+    if (existingUser) {
+      return res.render("./auth/sign-up", {
+        errorMessage: "Username taken",
+      });
+    }
+  });
+
+  bcrypt
+    .genSalt(salt)
+    .then((salt) => bcrypt.hash(password, salt))
+    .then((hashedPassword) => {
+      return User.create({
+        username: username,
+        password: hashedPassword,
+      });
+    })
+    .then((user) => {
+      console.log("new user", user);
+      res.redirect("/");
+    })
+    .catch((err) => console.log("err", err));
+});
+
+router.get("/login", (req, res) => {
+  res.render("./auth/login");
+});
+
+router.get("/userProfile", (req, res) => {
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
+
+router.post("/login", (req, res) => {
+  console.log("session ====>", req.session);
+  const { username, password } = req.body;
+  if (username === "" || password === "") {
+    res.render("/login", {
+      errorMessage: "Both fields required",
+    });
+    return;
+  }
+  User.findOne({ username }).then((user) => {
+    if (!user) {
+      res.render("/login", { errorMessage: "username not found" });
+      return;
+    } else if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user
+      res.redirect("/userProfile");
+    } else {
+      res.render("auth/login", { errorMessage: "wrong password" });
+    }
+  });
+});
 
 module.exports = router;
